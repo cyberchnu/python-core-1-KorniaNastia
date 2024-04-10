36fa7a4d63bc92447eb0fe4e118084d18be942cb
KorniaNastia <159483990+KorniaNastia@users.noreply.github.com>
2024-04-10 16:41:09 +0300

    task(makes 10)

--- src/makes10.py
+++ src/makes10.py
@@ -1,6 +1,5 @@
-def makes10(a, b):
-  if a == 10 or b == 10 or a + b == 10:
-    return True
-  else:
-    return False
+def makes10(number1, number2):
+    if number1 == 10 or number2 == 10 or number1 + number2 == 10:
+        return True
 
+print(makes10(1, 0))
