2010f3ddfc6888a3183a4eda581acc77b956fd0f
KorniaNastia <159483990+KorniaNastia@users.noreply.github.com>
2024-04-10 16:41:54 +0300

    task(squares sum)

--- src/squares_sum.py
+++ src/squares_sum.py
@@ -1,5 +1,7 @@
 def squares_sum(n):
-  result = 0
-  for i in range(1, n + 1):
-    result = result + i ** 2
-  return result
+    k = 0
+    for i in range(1, n + 1):
+        k += i ** 2
+    return k
+
+print(squares_sum(2))
