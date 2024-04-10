e2b249a583fff3f1049a729453eb56449b9abae9
KorniaNastia <159483990+KorniaNastia@users.noreply.github.com>
2024-04-10 16:38:26 +0300

    task(calculate)

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,7 +1,10 @@
 def calculate_fuel(distance):
-   fuel = distance * 10
+    fuel = distance * 10
     if distance < 0:
         return False
     elif fuel < 100:
         return 100
+
     return fuel
+
+print(calculate_fuel(20))
