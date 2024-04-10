a0aca32a92e22e849015a9f12e2130f5e9959b15
KorniaNastia <159483990+KorniaNastia@users.noreply.github.com>
2024-04-10 16:40:05 +0300

    task(int within bounds)

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,7 +1,9 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  if type(number) is float:
-    return False
-  elif number>lower_bound and number<upper_bound:
-    return True
-  else:
-    return False
+    if type(number) != int:
+        return False
+    elif number < upper_bound and number >= lower_bound:
+        return True
+    else:
+        return False
+
+print(int_within_bounds(1, 1, 3))
