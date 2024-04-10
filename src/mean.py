463ca221900fdacab587acf012596ab689d89e85
KorniaNastia <159483990+KorniaNastia@users.noreply.github.com>
2024-04-10 16:43:36 +0300

    task(mean)

--- src/mean.py
+++ src/mean.py
@@ -1,1 +1,13 @@
 def mean(number):
+    sum = 0
+    count = 0
+    str_number = str(number)
+    for letter in str_number:
+       d = int(letter)
+       sum += d
+       count += 1
+       mean = sum / count
+    return mean
+#
+#
+print(mean(794))
