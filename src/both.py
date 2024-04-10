3a648c5b3d7a0bcda2190eabca523091c2364020
KorniaNastia <159483990+KorniaNastia@users.noreply.github.com>
2024-04-10 16:39:14 +0300

    task(both)

--- src/both.py
+++ src/both.py
@@ -1,9 +1,7 @@
 def both(number1, number2):
+    if number1 > 0 and number2 > 0 or number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0:
+        return True
+    else:
+        return False
 
-  if number1 > 0 and number2 > 0 or number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0:
-#         return True
-#     else:
-#         return False
-
-#         print(both(0, 0))
-
+print(both(0, 0))
